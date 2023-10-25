04a46f801e1a34f624a8dacdb5deb47341267814
annabobby <bobbyanna433@gmail.com>
2023-10-25 15:36:36 +0530

    End of section 2

---
+++ -- SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"Bob");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"Tom");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"Bobby");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4,"Anna");
